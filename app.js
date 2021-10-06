01ecc0ab5f69be1cbfedc4a539e69700420d2034
lou duncan <31244585+dannyvsm@users.noreply.github.com>
2021-10-06 14:51:23 +0100

    Updated app.js with outline for EventListener

---
+++ app.js
@@ -0,0 +1,46 @@
+
+
+document.addEventListener('keydown', (event) => {
+    if (event.code == KeyA) {
+        //play audio
+    }
+    else if (event.code == KeyS) {
+        //play audio
+    }
+    else if (event.code == KeyD) {
+        //play audio
+    }
+    else if (event.code == KeyF) {
+        //play audio
+    }
+    else if (event.code == KeyG) {
+        //play audio
+    }
+    else if (event.code == KeyH) {
+        //play audio
+    }
+    else if (event.code == KeyJ) {
+        //play audio
+    }
+    else if (event.code == KeyK) {
+        //play audio
+    }
+    else if (event.code == KeyL) {
+        //play audio
+    }
+})
+
+// document.addEventListener('click', (event) => {
+//     let boom = document.getElementsByClassName("boom")
+//     let clap = document.getElementsByClassName("clap")
+//     let hihat = document.getElementsByClassName("hihat")
+//     let kick = document.getElementsByClassName("kick")
+//     let openhat = document.getElementsByClassName("openhat")
+//     let ride = document.getElementsByClassName("ride")
+//     let snare = document.getElementsByClassName("snare")
+//     let tink = document.getElementsByClassName("tink")
+//     let tom = document.getElementsByClassName("tom")
+
+//     //check whether button is clicked and then do actions
+//     //link audio autoplay on click
+// })
